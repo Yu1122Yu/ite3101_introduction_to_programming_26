2696b097fe8a0a79a427d2bbfdc1fc1547003cea
Lam Chi Ying <guodianjia35@gmail.com>
2026-09-11 12:05:13 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -2,7 +2,7 @@ january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
 annual_rainfall = january_to_june_rainfall
 
 july_rainfall = 1.05
-annual_rainfall += july_rainfall
+annual_rainfall += annual_rainfalljuly_rainfall
 
 august_rainfall = 4.91
 annual_rainfall += august_rainfall
