ffccd46c1bf76c893c668609195f01868edee320
Lam Chi Ying <guodianjia35@gmail.com>
2026-09-16 16:07:19 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -4,6 +4,6 @@ exercises_completed = 13
 points_per_exercise = 5
 
 point_total = 100
-point_total += exercises_completed * points_per_exercis
+point_total += exercises_completed * points_per_exercise
 
 print("I got " +str(point_total)+" points!")
