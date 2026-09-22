d72a8c7434d051c77b5d8e5b10ab9bff0e06869a
Lam Chi Ying <guodianjia35@gmail.com>
2026-09-22 16:17:19 +0000

    [skip actions] lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -3,7 +3,8 @@ pyg = 'ay'
 original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
-    word = original.lower(
-    )
+    word = original.lower()
+    Firs
+    
 else:
     print('empty')
