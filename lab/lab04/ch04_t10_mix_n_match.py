5b7cc44f972505dd2e8af2bcef80d78c31fd7b5b
Lam Chi Ying <guodianjia35@gmail.com>
2026-09-22 15:25:20 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -10,7 +10,7 @@ bool_two = (2 <= 2) or "Alpha" == "Bravo"
 bool_three = (2 <= 2) and "Alpha" == "Bravo"
 
 # Make me true!
-bool_four = None
+bool_four = (2 <= 2) and "Alpha" == "Bravo"
 
 # Make me true!
 bool_five = None
