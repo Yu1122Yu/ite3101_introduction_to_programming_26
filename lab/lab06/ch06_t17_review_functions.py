44b67ff43156cccc768ea096649da4e21f24d2c2
Lam Chi Ying <guodianjia35@gmail.com>
2026-09-25 14:18:04 +0000

    [skip actions] lab/lab06/ch06_t17_review_functions.py

--- lab/lab06/ch06_t17_review_functions.py
+++ lab/lab06/ch06_t17_review_functions.py
@@ -1,7 +1,2 @@
-def shut_down(s: str)-> str:
-    if s =="yes":
-        return "Shutting down"
-    eliif s=="no":
-        return "Shutting aborted"
-    else:
-        return "Sorry"
+def shut_down(s: str) -> str:
+    if s == "yes"
