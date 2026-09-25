2d5f3a12d90fcf8f141c1082bebed3e23a9da3e8
Lam Chi Ying <guodianjia35@gmail.com>
2026-09-25 12:04:06 +0000

    [skip actions] lab/lab06/ch06_t06_prac_makes_perfect.py

--- lab/lab06/ch06_t06_prac_makes_perfect.py
+++ lab/lab06/ch06_t06_prac_makes_perfect.py
@@ -1,11 +1,3 @@
 
 
 
-def cube(number: int) -> int:
-    return number * number * number
-
-    def by_three(number:int)-> Any:
-        if number % 3 ==0:
-            return cube(number)
-        else:
-            return False
